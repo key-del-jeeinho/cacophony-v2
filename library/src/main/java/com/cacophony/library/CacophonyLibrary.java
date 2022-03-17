278a85e08ffa4d30583306c5d256637590d99c3f
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-03-18 08:11:45 +0900

    [ADD] init시 Generator 바인딩을 위해 CacophonyLibrary에 static 변수 작성

--- library/src/main/java/com/cacophony/library/CacophonyLibrary.java
+++ library/src/main/java/com/cacophony/library/CacophonyLibrary.java
@@ -1,6 +1,14 @@
 package com.cacophony.library;
 
+import com.cacophony.library.rule.action.ActionGenerator;
+import com.cacophony.library.rule.flow.FlowGenerator;
+import com.cacophony.library.rule.trigger.TriggerGenerator;
+
 public class CacophonyLibrary {
+    private static ActionGenerator actionGenerator;
+    private static TriggerGenerator triggerGenerator;
+    private static FlowGenerator flowGenerator;
+
     public static void run() {
 
     }
