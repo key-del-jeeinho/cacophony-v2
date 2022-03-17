649dc206bc53539d4ba198861cab7175e5fffd9c
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-03-17 20:46:19 +0900

    [ADD] trigger 계층에 global 스펙 구현

---
+++ library/src/main/java/com/cacophony/library/rule/trigger/TriggerGenerateStrategy.java
@@ -0,0 +1,7 @@
+package com.cacophony.library.rule.trigger;
+
+import com.cacophony.library.global.GenerateStrategy;
+
+@FunctionalInterface
+public interface TriggerGenerateStrategy  extends GenerateStrategy<TriggerGenerator> {
+}
---
+++ library/src/main/java/com/cacophony/library/rule/trigger/TriggerGenerator.java
@@ -0,0 +1,10 @@
+package com.cacophony.library.rule.trigger;
+
+import com.cacophony.library.global.GenerateStrategy;
+import com.cacophony.library.global.SimpleGenerator;
+
+public class TriggerGenerator extends SimpleGenerator<Trigger> {
+    public TriggerGenerator(GenerateStrategy<Trigger> generateStrategy) {
+        super(generateStrategy);
+    }
+}
