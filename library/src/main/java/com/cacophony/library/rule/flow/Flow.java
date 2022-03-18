1100599d028a9a8aa1d925d5a7afb6bae0b50141
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-03-18 10:34:16 +0900

    [FIX] Flow를 interface로 수정

--- library/src/main/java/com/cacophony/library/rule/flow/Flow.java
+++ library/src/main/java/com/cacophony/library/rule/flow/Flow.java
@@ -2,18 +2,11 @@ package com.cacophony.library.rule.flow;
 
 import com.cacophony.library.rule.action.Action;
 import com.cacophony.library.rule.trigger.Trigger;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@Getter
-@RequiredArgsConstructor
-public abstract class Flow {
-    private final Trigger trigger;
-    private final Action action;
+public interface Flow {
+    Flow when(Trigger trigger);
+    Flow doAction(Action action);
+    Flow complete();
 
-    public abstract Flow when(Trigger trigger);
-    public abstract Flow doAction(Action action);
-    public abstract Flow complete();
-
-    public abstract void execute(FlowRequest e);
+    void execute(FlowRequest e);
 }
