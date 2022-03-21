33065a8b43f8f2d1ffea9fdf6b46a2625dd28315
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2022-03-21 21:09:37 +0900

    [ADD Entry 작성

---
+++ library/src/main/java/com/cacophony/library/domain/event/entry/EventActionEntry.java
@@ -0,0 +1,45 @@
+package com.cacophony.library.domain.event.entry;
+
+import com.cacophony.library.domain.event.action.EventAction;
+import com.cacophony.library.domain.event.action.parameter.EventActionParameter;
+import com.cacophony.library.domain.event.common.Event;
+import com.cacophony.library.domain.event.common.StaticEventConfiguration;
+import lombok.RequiredArgsConstructor;
+
+import java.util.function.Consumer;
+
+@RequiredArgsConstructor
+public class EventActionEntry {
+    private final Consumer<Event> function;
+
+    public static EventAction doActionComplete(Consumer<Event> function) {
+        return complete(function);
+    }
+
+    public static EventActionEntry doAction(Consumer<Event> function) {
+        return generate(function);
+    }
+
+    public EventActionEntry and(Consumer<Event> function) {
+        return generate(this.function.andThen(function));
+    }
+
+    public EventAction complete() {
+        return complete(function);
+    }
+
+    private static EventAction complete(Consumer<Event> function) {
+        return StaticEventConfiguration.generateAction()
+                .builder()
+                .function(parameter -> function.accept(getEvent(parameter)))
+                .build();
+    }
+
+    private static EventActionEntry generate(Consumer<Event> function) {
+        return new EventActionEntry(function);
+    }
+
+    private static Event getEvent(EventActionParameter parameter) {
+        return parameter.getParameter("event", Event.class);
+    }
+}
---
+++ library/src/main/java/com/cacophony/library/domain/event/entry/EventTriggerEntry.java
@@ -0,0 +1,53 @@
+package com.cacophony.library.domain.event.entry;
+
+import com.cacophony.library.domain.event.common.*;
+import com.cacophony.library.domain.event.trigger.EventTrigger;
+import com.cacophony.library.domain.event.trigger.parameter.EventTriggerParameter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.function.Function;
+
+@RequiredArgsConstructor
+public class EventTriggerEntry {
+    private final Function<EventTriggerParameter, EventTriggerParameter> function;
+
+    public static <T extends Event> EventTrigger onComplete(Class<T> event) {
+        return complete(first().andThen(then(event)));
+    }
+
+    public static <T extends Event> EventTriggerEntry on(Class<T> event) {
+        return generate(first(), event);
+    }
+
+    public <T extends Event> EventTriggerEntry and(Class<T> event) {
+        return generate(function, event);
+    }
+
+    public EventTrigger complete() {
+        return complete(function);
+    }
+
+    private static Function<EventTriggerParameter, EventTriggerParameter> first() {
+        return parameter -> parameter.addParameter("isTriggerActivated", false);
+    }
+
+    private static <T extends Event> Function<EventTriggerParameter, EventTriggerParameter> then(Class<T> event) {
+        return parameter -> {
+            if(parameter.getParameter("isTriggerActivated", Boolean.class)) return parameter;
+            if(event.isInstance(parameter.getParameter("event")))
+                parameter.addParameter("isTriggerActivated", true);
+            return parameter;
+        };
+    }
+
+    private static EventTrigger complete(Function<EventTriggerParameter, EventTriggerParameter> function) {
+        return StaticEventConfiguration.generateTrigger()
+                .builder()
+                .trigger(function.andThen(parameter -> parameter.getParameter("isTriggerActivated", Boolean.class)))
+                .build();
+    }
+
+    private static <T extends Event> EventTriggerEntry generate(Function<EventTriggerParameter, EventTriggerParameter> origin, Class<T> event) {
+        return new EventTriggerEntry(origin.andThen(then(event)));
+    }
+}
